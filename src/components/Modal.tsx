ab6c94e78f13a8d8eb0b9dad10060ab35d7a302f
Paulo <paulocorreapc93@gmail.com>
2023-08-28 14:28:38 -0400

    criado evento para fechar modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,11 +5,15 @@ type Props = {
   children: React.ReactNode;
 };
 
-const Modal = ({children}: Props) => {
+const Modal = ({ children }: Props) => {
+  const closeModal = (e: React.MouseEvent): void => {
+    const modal = document.querySelector("#modal");
+    modal!.classList.add("hide");
+  };
 
   return (
     <div id="modal">
-      <div className={style.fade}></div>
+      <div className={style.fade} onClick={closeModal}></div>
       <div className={style.modal}>
         <h2>Texto Modal</h2>
         {children}
